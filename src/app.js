440ec131841578b2b16ca5646d7f057b9c4e4929
Akshar Pathak <aksharpathak896@gmail.com>
2026-08-14 22:23:55 +0530

    added some routers like /test in app.use

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 const express=require('express');
 const app=express();
-app.use((req,res)=>{
-    res.send("Hello from the server bro");
+app.use("/hello",(req,res)=>{
+    res.send("Hello from the hello server bro");
+});
+app.use("/test",(req,res)=>{
+    res.send("Hello from the test server bro");
 });
 app.listen(3000, () => {
     console.log("Server is listening on the port 3000");
