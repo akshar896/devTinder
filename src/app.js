8241a0f013950a8ef2226fe562072385a4577b6d
Akshar Pathak <aksharpathak896@gmail.com>
2026-08-16 17:32:18 +0530

    used app.use at the start to check the behaviour of api responses

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 const express=require('express');
 const app=express();
 
+app.use("/user",(req,res)=>{
+    res.send("blablabla");
+})
+//order matters my friend in javascript...since now you are using app.use so it will
+//be accountable for all the methods....
 app.get("/user",(req,res)=>{
     res.send("Getting user data");
 })
+
 
 app.post("/user",(req,res)=>{
     res.send("User data successfully saved");
