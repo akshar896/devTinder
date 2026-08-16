46251cfa60879e44e1443d722af99a2bb508559d
Akshar Pathak <aksharpathak896@gmail.com>
2026-08-16 16:31:03 +0530

    changed the position of routers to check the behaviour

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
 const express=require('express');
 const app=express();
-app.use("/",(req,res)=>{
-    res.send("Just /")
-})
 app.use("/hello",(req,res)=>{
     res.send("Hello hello hello");
 });
 app.use("/test",(req,res)=>{
     res.send("test test test");
 });
+app.use("/",(req,res)=>{
+    res.send("Just /")
+})
 app.listen(3000, () => {
     console.log("Server is listening on the port 3000");
 });
