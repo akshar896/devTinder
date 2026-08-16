874416ee7d548a70b347bee92069ca9aaee11b76
Akshar Pathak <aksharpathak896@gmail.com>
2026-08-16 17:43:27 +0530

    /user/akshar get method use

--- src/app.js
+++ src/app.js
@@ -1,15 +1,14 @@
 const express=require('express');
 const app=express();
 
-app.use("/user",(req,res)=>{
-    res.send("blablabla");
-})
-//order matters my friend in javascript...since now you are using app.use so it will
-//be accountable for all the methods....
 app.get("/user",(req,res)=>{
     res.send("Getting user data");
 })
 
+
+app.get("/user/akshar",(req,res)=>{
+    res.send("Getting user data for akshar");
+})
 
 app.post("/user",(req,res)=>{
     res.send("User data successfully saved");
@@ -22,6 +21,6 @@ app.delete("/user",(req,res)=>{
 app.use("/",(req,res)=>{
     res.send("Just /")
 })
-app.listen(3000, () => {
-    console.log("Server is listening on the port 3000");
+app.listen(7777, () => {
+    console.log("Server is listening on the port 7777");
 });
