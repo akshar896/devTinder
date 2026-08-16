022cd2a7730dddf762804a758cc0fc20b7365426
Akshar Pathak <aksharpathak896@gmail.com>
2026-08-16 16:19:48 +0530

    Playing with routes, order of the routes matter

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 const express=require('express');
 const app=express();
+app.use("/",(req,res)=>{
+    res.send("Just /")
+})
 app.use("/hello",(req,res)=>{
-    res.send("Hello from the hello server bro");
+    res.send("Hello hello hello");
 });
 app.use("/test",(req,res)=>{
-    res.send("Hello from the test server bro");
+    res.send("test test test");
 });
 app.listen(3000, () => {
     console.log("Server is listening on the port 3000");
