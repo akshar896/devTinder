16403b9cda839eb5790db7ac71d9be82ecb660f0
Akshar Pathak <aksharpathak896@gmail.com>
2026-08-17 20:08:22 +0530

    params and query concept

--- src/app.js
+++ src/app.js
@@ -1,26 +1,25 @@
 const express=require('express');
 const app=express();
-
+//query (when we are give some query in the request url, we can
+// access it through req.query)
 app.get("/user",(req,res)=>{
-    res.send("Getting user data");
+    console.log(req.query);
+    res.send({
+        firstName:"Akshar",
+        lastName:"Pathak",
+    })
+})
+//param ,(When we have to do dynamic routing coz if there are 1 lakh users
+// in your application you wont add each name to your routes,so we make 
+// a dynamic route in that case by putting ":" sign)
+app.get("/user/:userId/:password",(req,res)=>{
+    console.log(req.params);
+    res.send({
+        firstName:"Akshar",
+        lastName:"Pathak",
+    })
 })
 
-
-app.get("/user/akshar",(req,res)=>{
-    res.send("Getting user data for akshar");
-})
-
-app.post("/user",(req,res)=>{
-    res.send("User data successfully saved");
-})
-
-app.delete("/user",(req,res)=>{
-    res.send("user data deleted successfully");
-})
-
-app.use("/",(req,res)=>{
-    res.send("Just /")
-})
 app.listen(7777, () => {
     console.log("Server is listening on the port 7777");
 });
