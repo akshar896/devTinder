af171229875c98183382a2387f0520cbed6d7267
Akshar Pathak <aksharpathak896@gmail.com>
2026-08-18 20:52:14 +0530

    more on ROuter handlers

--- src/app.js
+++ src/app.js
@@ -4,21 +4,16 @@ const app=express();
 app.use("/user",(req,res,next)=>{
     console.log("Route handler 1");
     next();
+    res.send("Route Handler 1")
 },(req,res,next)=>{
     res.send("ROute Handler 2");
-    next();
-},(req,res)=>{
-    res.send("ROute Handler 2");
 })
-// in this case at first it goes to route handler 1
-//since there is no response it goes to next(), 'next' parameter 
-//must be passed in the Route handler in order to use this parameter
-//then it goes to route handler 2 and send the response to the user
-//but it will still go to the ROute handler 3 as javascript goes line 
-//by line, so it will go to the third router but since the response is 
-//already sent to the user it wont send any response again, but since there
-// is one more reponse for the same route("/user"), it gives error, not
-//to the user but to the developer.....
+//now it will return Route handler 2...and the reason you know...
+//1:it goes to the RH1...(route handler 1)
+//As Javscript goes line by line...it goes to the next router using next() function
+//then it sends ROute hadnler 2 as the response...but since RH1 is not completed yee
+//it tries to res.send(Router handler 1) and gives the error to the developer side...
+
 
 app.listen(7777, () => {
     console.log("Server is listening on the port 7777");
