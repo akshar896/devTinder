15a429cc7454a3ce7e037b467cb1b87c431b497f
Akshar Pathak <aksharpathak896@gmail.com>
2026-08-18 20:40:40 +0530

    empty route handler

--- src/app.js
+++ src/app.js
@@ -1,24 +1,12 @@
 const express=require('express');
 const app=express();
-//query (when we are give some query in the request url, we can
-// access it through req.query)
-app.get("/user",(req,res)=>{
-    console.log(req.query);
-    res.send({
-        firstName:"Akshar",
-        lastName:"Pathak",
-    })
+
+app.use("/user",(req,res)=>{
+    console.log("Hello user");
 })
-//param ,(When we have to do dynamic routing coz if there are 1 lakh users
-// in your application you wont add each name to your routes,so we make 
-// a dynamic route in that case by putting ":" sign)
-app.get("/user/:userId/:password",(req,res)=>{
-    console.log(req.params);
-    res.send({
-        firstName:"Akshar",
-        lastName:"Pathak",
-    })
-})
+//when you don't respond anything then the server goes to infite loop
+//because it doesn't know what response to give to the user for this
+//particular route
 
 app.listen(7777, () => {
     console.log("Server is listening on the port 7777");
