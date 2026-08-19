1cbd0878fdb4f707ac78a986fb21fb433fccbc47
Akshar Pathak <aksharpathak896@gmail.com>
2026-08-19 22:07:06 +0530

    diff btween app.get and app.use calls

--- src/app.js
+++ src/app.js
@@ -1,17 +1,14 @@
 const express=require('express');
 const app=express();
 
-app.use("/user",(req,res)=>{
-    res.send("User data sent");
+app.get("/user",(req,res)=>{
+    res.send("getting the data of user1");
 })
-app.use("/user/123",(req,res)=>{
-    res.send("User123 data sent");
+app.get("/user/123",(req,res)=>{
+    res.send("getting the data of user123");
 })
-//here user data sent will be outputted because /user matches the prefix of
-//url /user/123....Hence this will be called for all the url's matching the 
-//prefix /user...this is how routing in exrpess works...so we can use app.use
-//as a middleware because every url matching the /user will first go to this 
-//app.use("/user")..hence we can put whatever conditions we want like auth...
+//here for /user first response will be sent and for /user/123 second...
+//in app.get methods there is not prefix matching....
 
 app.listen(7777, () => {
     console.log("Server is listening on the port 7777");
