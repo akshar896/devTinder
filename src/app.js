8c8523481602810ecdc6703a34c2f9ca2b4ba8b0
Akshar Pathak <aksharpathak896@gmail.com>
2026-08-19 21:58:46 +0530

    app.use as a middleware

--- src/app.js
+++ src/app.js
@@ -1,19 +1,17 @@
 const express=require('express');
 const app=express();
 
-app.use("/user",(req,res,next)=>{
-    console.log("Route handler 1");
-    next();
-    res.send("Route Handler 1")
-},(req,res,next)=>{
-    res.send("ROute Handler 2");
+app.use("/user",(req,res)=>{
+    res.send("User data sent");
 })
-//now it will return Route handler 2...and the reason you know...
-//1:it goes to the RH1...(route handler 1)
-//As Javscript goes line by line...it goes to the next router using next() function
-//then it sends ROute hadnler 2 as the response...but since RH1 is not completed yee
-//it tries to res.send(Router handler 1) and gives the error to the developer side...
-
+app.use("/user/123",(req,res)=>{
+    res.send("User123 data sent");
+})
+//here user data sent will be outputted because /user matches the prefix of
+//url /user/123....Hence this will be called for all the url's matching the 
+//prefix /user...this is how routing in exrpess works...so we can use app.use
+//as a middleware because every url matching the /user will first go to this 
+//app.use("/user")..hence we can put whatever conditions we want like auth...
 
 app.listen(7777, () => {
     console.log("Server is listening on the port 7777");
