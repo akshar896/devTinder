e3b93b5c49e91de5b6d7eef0388f4a7795c896f1
Akshar Pathak <aksharpathak896@gmail.com>
2026-08-21 16:11:42 +0530

    middleware concept2

--- src/app.js
+++ src/app.js
@@ -2,25 +2,22 @@ const express=require('express');
 const app=express();
 
 //now we will see the use of middlewares in auth
-app.get("/user/profile",(req,res,next)=>{
-    const token="abc";
+app.use("/user/profile",(req,res,next)=>{
+    const token="xbc";
     const authToken="abc";
     if(token===authToken){
         next();
     }
     else{
         res.status(401).send("Unauthorised access");
-        
     }
 })
 app.get("/user/profile",(req,res)=>{
     res.send("Getting the user profile");
 })
-
-// now in this case the user will be authorised first before getting the data..
-//This is the different way of writing routes...first one we studied in the same
-//routes we keep adding more and more route handlers..but instead of that we
-//can create one more same routes and it will be giving the same results
+//app.use can be considered as an inbuilt middleware😂
+//coz it will always authenticate any route with prefix /user/profile
+//unlike app.get() coz it doesn't do prefix matching
 
 app.listen(7777, () => {
     console.log("Server is listening on the port 7777");
