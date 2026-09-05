a7cb8a7e679851143ad97d79219d6a918b2d5561
Akshar Pathak <aksharpathak896@gmail.com>
2026-09-05 14:42:13 +0530

    Creating login api

--- src/app.js
+++ src/app.js
@@ -26,36 +26,27 @@ app.post("/signup",async (req,res)=>{
     }
 })
 
-app.patch("/update/:userId/",async (req,res)=>{
-    const userId = req.params?.userId;
-    const data=req.body;
+app.post("/login",async (req,res)=>{
     try{
-        const allowedUpdates=["gender","age","skills","about"];
-        const isAllowedUpdates=Object.keys(data).every((k)=>allowedUpdates.includes(k));
-        if(!isAllowedUpdates){
-            throw new Error("Update not allowed");
+        //first validate the email
+        const {emailId,password}=req.body;
+        const user=await User.findOne({emailId});
+        if(!user){
+            throw new Error("Email id doesn't exists");
         }
-        if(data?.skills.length>10){
-            throw new Error("Skills can't me more than 10");
+        //then compare the password and hash password
+        const isPasswordValid=await bcrypt.compare(password,user.password);
+        if(isPasswordValid){
+            res.send("Logged in successfully");
         }
-        await User.findByIdAndUpdate({_id:userId},data,{runValidators:true});
-        res.send("User updated successfully");
+        else{
+            throw new Error("Invalid password");
+        }
     }
     catch(err){
-        res.status(400).send("Error saving the data "+err.message);
+        res.status(400).send("Error:"+err.message);
     }
 })
-// // In this section, we are using validators to make our schema more robust.
-
-// Note:
-// When signing up a new user, validators are automatically applied
-// when user.save() is called.
-//
-// However, when updating an existing user using methods like
-// findByIdAndUpdate(), Mongoose does not run validators by default.
-//
-// Therefore, we need to explicitly pass { runValidators: true }
-// if we want schema validators to run during the update as well.
 
 connectDB().then(()=>{
     console.log("Database connection established");
